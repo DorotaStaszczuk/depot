dd833ecc8d8b8bf0fa8831cfc5c6dec1efabf07b
Dorota Staszczuk <dorota.staszczuk@gmail.com>
2018-11-15 17:03:12 +0100

    Catalog Display

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,9 @@ class Product < ActiveRecord::Base
   validates :image_url, allow_blank: true, format: { with: %r{\.(gif|jpg|png)\Z}i,
 message: 'must be a URL for GIF, JPG or PNG image.'
 }
+validates :title, length: {minimum: 10}
+
+  def self.latest
+    Product.order(:updated_at).last
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,6 @@ Depot::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.action_controller.perform_caching = false
 end
