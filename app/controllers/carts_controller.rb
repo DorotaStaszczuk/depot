a096ccd9f994b5c4809981d55e95310f7bd60e8d
Dorota Staszczuk <dorota.staszczuk@gmail.com>
2018-11-21 13:09:26 +0100

    sending confirmation emails

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -55,7 +55,7 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
-    @cart.destroy if @cart.id == session[:cart_id]
+    @cart.destroy if @cart.id == session[:id]
     session[:cart_id] = nil
     respond_to do |format|
       format.html { redirect_to store_url }
