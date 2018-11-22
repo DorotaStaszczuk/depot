5a964fc8e356dcec2cf250724498e3757793769c
Dorota Staszczuk <dorota.staszczuk@gmail.com>
2018-11-22 10:15:19 +0100

    selecting the locale and translating the storefront

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,34 @@
 Depot::Application.routes.draw do
   get 'admin' => 'admin#index'
+
   controller :sessions do
-    get 'login' => :new
+    get  'login' => :new
     post 'login' => :create
     delete 'logout' => :destroy
   end
 
   get "sessions/create"
   get "sessions/destroy"
+
   resources :users
 
-  resources :orders
-
-  resources :line_items
-
-  resources :carts
-
-  get "store/index"
   resources :products do
     get :who_bought, on: :member
   end
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store', via: :all
+  end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'store#index', as: 'store'
+  #root 'store#index', as: 'store'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
